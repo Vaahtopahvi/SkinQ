895f455389afbae3b9a38fe804e1e430eb51b46f
Riku <riku.vaatanen@gmail.com>
2022-03-17 16:23:30 +0200

    add shit

---
+++ functionality.js
@@ -0,0 +1,21 @@
+function browseList() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    dropdown = document.getElementById("myDropdown");
+    if (input.value.trim() != '') {
+      dropdown.classList.add('show');
+    } else {
+      dropdown.classList.remove('show');
+    }
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
